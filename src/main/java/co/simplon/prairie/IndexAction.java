9ed0410764e8b8ec6d7dd63b6eadf9ae39a07634
Eric Siber <eric.siber@gmail.com>
2016-10-26 15:34:55 +0200

    Init

---
+++ src/main/java/co/simplon/prairie/IndexAction.java
@@ -0,0 +1,19 @@
+package co.simplon.prairie;
+
+import jodd.madvoc.meta.Action;
+import jodd.madvoc.meta.MadvocAction;
+import jodd.madvoc.meta.Out;
+
+import java.util.List;
+
+@MadvocAction
+public class IndexAction {
+
+    @Out
+    String prenom;
+
+    @Action
+    public void view() {
+        prenom = "Eric";
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<html>
+<body>
+<h1>Mon application Web en Java</h1>
+
+Ceci est une page statique.
+
+</body>
+</html>
